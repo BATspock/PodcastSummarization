de8d811afbbbda94455d8bb0ed449544da397795
Aditya Kishore <aditya20kishore@gmail.com>
2023-02-04 20:59:51 -0800

    summarization complete

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,11 @@ from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
 import whisper
+from transformers import pipeline
 
 app = Flask(__name__)
-
+summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
+model = whisper.load_model("tiny.en")
 
 @app.route('/', methods=["GET"])
 def index():
@@ -24,7 +26,7 @@ def index():
 
 def download():
     # get url from request
-    print("Request method: ", request.method)
+    #print("Request method: ", request.method)
     if request.method == "GET":
         # print(request.args.get("url"))
         url = request.args.get("url")
@@ -37,23 +39,33 @@ def download():
         #     "preferredquality": "192",
         #     }],
         }
-        print("Downloading audio now\n")
+        print("########################## Downloading audio now #####################\n")
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        print("Download completed!")
+        print("########################## Download completed! #####################\n")
         # upload to S3 bucket
         # or upload to MongoDB
         # add to queue
         # for implemenation later
 
-        print("Transcribing audio now\n")
-        model = whisper.load_model("tiny.en")
-        result = model.transcribe("file.mp3")
-        print(result["text"])
+        print("######################### Transcribing audio now #####################\n")
+        
+        result = model.transcribe("file.mp3", fp16=False)
+        #print(result["text"])
         # save trascrtiption to a text file
         with open("transcription.txt", "w") as f:
             f.write(result["text"])
-        print("Transcription completed!")
+        print("######################### Transcription completed! #####################\n")
+
+        # summarize the transcription
+        print("###################### Summarizing transcription now ###################\n")
+        summary = summarizer(result["text"], max_length=2000, min_length=100, do_sample=False)
+        print(summary[0]["summary_text"])
+        print("######################### Summarization completed! #####################\n")
+        # save summary to a text file
+        with open("summary.txt", "w") as f:
+            f.write(summary[0]["summary_text"])
+
     return jsonify({"message": "success"})
 
 
