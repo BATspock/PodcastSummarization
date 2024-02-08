b13e4a52142e288d10c807f3797c82892046f851
Aditya Kishore <adityaki@usc.edu>
2024-02-07 23:04:20 -0800

    added description in readme

---
+++ MiscResources/fasterWhisper.py
@@ -0,0 +1,20 @@
+import torch
+from transformers import pipeline
+from transformers.utils import is_flash_attn_2_available
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model="openai/whisper-large-v3", # select checkpoint from https://huggingface.co/openai/whisper-large-v3#model-details
+    torch_dtype=torch.float16,
+    device="cuda:0", # or mps for Mac devices
+    model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
+)
+
+outputs = pipe(
+    "audio.mp3",
+    chunk_length_s=30,
+    batch_size=24,
+    return_timestamps=True,
+)
+
+print(outputs)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PodcastSummarization
 Project to summarize podcast via urls
+
+To run the project backend, run the following commands:
+```
+python main.py
+```
+
+Example of a request to the backend:
+```
+http://127.0.0.1:5000/transcribe?url=https://www.youtube.com/watch?v=cjwpxzXlpC8&ab_channel=LexClips
+```
